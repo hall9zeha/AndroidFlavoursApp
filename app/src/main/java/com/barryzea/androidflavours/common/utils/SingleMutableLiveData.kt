40652fb5d29a7690da011ce0b147001a4d24c416
hall9zeha <vmwaretars@gmail.com>
2024-02-04 11:34:56 -0500

    Added mutableLiveData custom

--- app/src/main/java/com/barryzea/androidflavours/common/utils/SingleMutableLiveData.kt
+++ app/src/main/java/com/barryzea/androidflavours/common/utils/SingleMutableLiveData.kt
@@ -1,9 +1,23 @@
 package com.barryzea.androidflavours.common.utils
 
+import androidx.annotation.MainThread
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Observer
+
 /**
  * Project AndroidFlavours
  * Created by Barry Zea H. on 04/02/2024.
  **/
 
-class SingleMutableLiveData{
+class SingleMutableLiveData<T>:MutableLiveData<T>(){
+    @MainThread
+    override fun observe(owner: LifecycleOwner, observer: Observer<in T>) {
+        super.observe(owner) { t ->
+            if (t != null) {
+                observer.onChanged(t)
+                postValue(null)
+            }
+        }
+    }
 }
