af960a38517cb737877a72017c826fb7673260d2
hall9zeha <vmwaretars@gmail.com>
2024-02-16 12:33:04 -0500

    Added PrefsEntity

---
+++ app/src/main/java/com/barryzea/androidflavours/data/entities/PrefsEntity.kt
@@ -0,0 +1,9 @@
+package com.barryzea.androidflavours.data.entities
+
+/**
+ * Project AndroidFlavours
+ * Created by Barry Zea H. on 16/02/2024.
+ **/
+data class PrefsEntity(
+    val sessionId:String?=""
+)
