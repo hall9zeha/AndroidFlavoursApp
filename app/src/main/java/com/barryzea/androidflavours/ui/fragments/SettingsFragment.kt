6ddc527318ebcc1786ccde43c9d9bb889b53af61
hall9zeha <vmwaretars@gmail.com>
2024-02-29 13:19:05 -0500

    Added abouth this dialog

--- app/src/main/java/com/barryzea/androidflavours/ui/fragments/SettingsFragment.kt
+++ app/src/main/java/com/barryzea/androidflavours/ui/fragments/SettingsFragment.kt
@@ -3,10 +3,14 @@ package com.barryzea.androidflavours.ui.fragments
 import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatDelegate
+import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
 import androidx.preference.SwitchPreferenceCompat
+import com.barryzea.androidflavours.BuildConfig
 import com.barryzea.androidflavours.R
+import com.barryzea.androidflavours.databinding.AboutThisLayoutBinding
 import com.barryzea.androidflavours.ui.activities.MainActivity
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 
 class SettingsFragment : PreferenceFragmentCompat() {
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
@@ -16,6 +20,8 @@ class SettingsFragment : PreferenceFragmentCompat() {
     private fun setUpListeners(){
 
         val themePrefs = findPreference<SwitchPreferenceCompat>("darkTheme")
+        val aboutThisPref = findPreference<Preference>("aboutThis")
+
         themePrefs?.setOnPreferenceChangeListener { preference, newValue ->
             if(newValue as Boolean){
                 AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
@@ -24,8 +30,18 @@ class SettingsFragment : PreferenceFragmentCompat() {
             }
             true
         }
+        aboutThisPref?.setOnPreferenceClickListener {
+            showAboutThisDialog()
+            true
+        }
     }
-
+    private fun showAboutThisDialog(){
+        val dialogBind = AboutThisLayoutBinding.inflate(layoutInflater)
+        dialogBind.tvAboutThis.text= getString(R.string.about_this_msg,BuildConfig.VERSION_NAME)
+        MaterialAlertDialogBuilder(requireContext())
+            .setView(dialogBind.root)
+            .show()
+    }
     override fun onResume() {
         super.onResume()
         activity?.let {
